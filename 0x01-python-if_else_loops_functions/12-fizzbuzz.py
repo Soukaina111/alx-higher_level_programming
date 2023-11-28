f9767713cad1f4c9f178eb8022da7205c58d4b06
Soukaina111 <basmahajar@gmail.com>
2023-11-28 08:57:16 -0800

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    output = ""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            output += "FizzBuzz "
+        elif i % 3 == 0:
+            output += "Fizz "
+        elif i % 5 == 0:
+            output += "Buzz "
+        else:
+            output += "{} ".format(i)
+    print(output, end="")
