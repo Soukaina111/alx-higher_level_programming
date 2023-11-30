000d578d4afd0b3a5da3f0396ea02eb507dba117
Soukaina111 <basmahajar@gmail.com>
2023-11-30 07:36:20 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,15 +4,15 @@ import sys
 if __name__ != "__main__":
     exit()
 
-argStr = "{:d} argument"
+line = "{:d} argument"
 argc = len(sys.argv) - 1
 if argc == 0:
-    argStr += 's.'
+    line += 's.'
 elif argc == 1:
-    argStr += ':'
+    line += ':'
 else:
-    argStr += 's:'
-print(argStr.format(argc))
+    line += 's:'
+print(line.format(argc))
 
 i = 0
 for arg in sys.argv:
