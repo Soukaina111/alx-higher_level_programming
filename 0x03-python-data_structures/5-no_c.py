25b421da06c2755cea75a6da468ebf5a0f4edf8d
Soukaina111 <basmahajar@gmail.com>
2023-12-02 06:44:52 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
 	copy= my_string[:]
         for i in copy:
-                if (i=='c' or i =='C'):
-   			copy.remove(i)
-	return ("".join(copy))
+                if i=='c' or i =='C':
+                    copy.remove(i)
+	return (" ".join(copy))
