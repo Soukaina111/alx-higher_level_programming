aceb9742577f0201365e80dbe0fba60dc5457e08
Soukaina111 <basmahajar@gmail.com>
2023-12-02 07:28:48 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-	tuple1 = (0,None)
+        tuple1 = (0, None)
     else:
-        tuple1 = (len(sentence),sentence[:1])
-    return (tuple1)
+        tuple1 = (len(sentence), sentence[:1])
+    return(tuple1)
