a013c26eda9227f985ebbe7d2946833f41ba60df
Soukaina111 <basmahajar@gmail.com>
2023-12-18 07:43:54 -0800

    Fisrt task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    cpt = 0
-    sum = 0
-    for cpt in range(x):
+    i = 0
+    counted = 0
+    for i in range(x):
         try:
-            print("{}".format(my_list[cpt]), end="")
-            sum += 1
+            print("{}".format(my_list[i]), end="")
+            counted += 1
         except:
             continue
     print()
-    return sum
+    return counted
