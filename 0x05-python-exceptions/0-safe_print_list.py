e7948f508477bf52c8f9184a7ba61400ed75d41c
Soukaina111 <basmahajar@gmail.com>
2023-12-18 07:40:25 -0800

    Fisrt task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    cpt = 0
+    sum = 0
+    for cpt in range(x):
+        try:
+            print("{}".format(my_list[cpt]), end="")
+            sum += 1
+        except:
+            continue
+    print()
+    return sum
