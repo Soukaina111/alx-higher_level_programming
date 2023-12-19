b271717408bd0837276352388057d4a5abd5011f
Soukaina111 <basmahajar@gmail.com>
2023-12-19 07:23:30 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square """
+
+
+class Square:
+    """ class Square """
+    def __init__(self, size):
+        """initialize square
+        Args:
+            size (int): square size
+        """
+        self.__size = size
