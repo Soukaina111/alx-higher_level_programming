6b43b9109c53859df8981d25798970df120b1197
Soukaina111 <basmahajar@gmail.com>
2024-01-09 10:27:29 -0800

    Reda-file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""defining a text file-reading function"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as fi:
+    """shows the content of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as fi:
         print(fi.read(), end="")
