f4f05b61c7f5a3ab2715326dd276e149068af31f
Soukaina111 <basmahajar@gmail.com>
2024-01-15 11:14:06 -0800

    rectan3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,7 +21,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        self.validate_integer("width", value, False)
+        self.validate_inte("width", value, False)
         self.__width = value
 
     @property
@@ -31,7 +31,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        self.validate_integer("height", value, False)
+        self.validate_inte("height", value, False)
         self.__height = value
 
     @property
@@ -41,7 +41,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.validate_integer("x", value)
+        self.validate_inte("x", value)
         self.__x = value
 
     @property
@@ -51,10 +51,10 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        self.validate_integer("y", value)
+        self.validate_inte("y", value)
         self.__y = value
 
-    def validate_integer(self, name, value, eq=True):
+    def validate_inte(self, name, value, eq=True):
         '''Method to validate the value integer.'''
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
