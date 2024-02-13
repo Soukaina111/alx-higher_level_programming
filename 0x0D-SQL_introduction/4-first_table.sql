555fa8ece5b563135fb17d4eb9e70db68b44376e
Soukaina111 <basmahajar@gmail.com>
2024-02-13 08:55:17 -0800

    TASK6-7

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---This script creates a first_table in database if it doesn't exist
+-- This script creates a first_table in database if it doesn't exist
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
