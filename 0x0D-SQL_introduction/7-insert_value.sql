f6ac7db2e02b96be8cf00a11f7faae60540c7a5c
Soukaina111 <basmahajar@gmail.com>
2024-02-13 07:56:07 -0800

    TASK6-7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 --This is the first  Insertion in our table
--- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
