006359f32a0578916dff1ff38daf8a0e4d0b5d8f
Soukaina111 <basmahajar@gmail.com>
2024-02-14 07:18:49 -0800

    0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- This script Shows all the privilges granted for two users
-SHOW GRANTS FOR user_0d_1@localhost AND user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
 
