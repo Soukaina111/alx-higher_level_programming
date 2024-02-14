ae92995a56997b52505a49a5d1d3449b6d999b11
Soukaina111 <basmahajar@gmail.com>
2024-02-14 07:58:43 -0800

    tasks

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- This script creates a table in the db with its attributes
-CREATE table IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT = 1, name VARCHAR(256));
+CREATE table IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT 1, name VARCHAR(256));
