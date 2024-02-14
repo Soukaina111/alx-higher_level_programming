fcacfb39a1a30a13ab9c2dfd374d0ea9eeca971a
Soukaina111 <basmahajar@gmail.com>
2024-02-14 08:50:25 -0800

    tasks

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
--- This script for 13-count_shows_by_genre.sql
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-       FROM tv_show_genres
-       INNER JOIN tv_genres
-       ON tv_genres.id = tv_show_genres.genre_id
-       GROUP BY tv_genres.name
-       ORDER BY number_shows DESC;
+-- this script for task 13
+SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+GROUP BY tv_show_genres.genre_id
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
