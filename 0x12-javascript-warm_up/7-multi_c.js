b245f5efc09b2bc135b1171c590c434da0124691
Soukaina111 <basmahajar@gmail.com>
2024-03-11 10:19:43 -0700

    JS0

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// This Script Write a script that prints x times “C is fun” 
+// else it prints “Missing number of occurrences” in case the arg isNan
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
