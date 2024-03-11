0be3523a67f20f82f9dce8c9120d81b9c0af4d64
Soukaina111 <basmahajar@gmail.com>
2024-03-11 09:21:48 -0700

    JS0

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,9 @@
 // This Script checks the arg of the standard input
 
 if (process.argv.length === 2) {
-  console.log("No argument");
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-  console.log("Argument Found");
+  console.log('Arguments found');
 }
