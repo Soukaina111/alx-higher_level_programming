ff6935791b4d40724a30bb60f59cf2571b31aaaa
Soukaina111 <basmahajar@gmail.com>
2024-03-11 08:55:02 -0700

    JS

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// This script prints the content of myvar 
-// "Javascript is amazing"
+//This script prints the content of myvar Variable
 
 const myVar = "JavaScript is amazing";
 console.log(myVar);
