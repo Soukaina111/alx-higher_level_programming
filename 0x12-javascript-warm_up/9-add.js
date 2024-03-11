f95dc51dbae5452b8d2a5aa8439431f28bdff9db
Soukaina111 <basmahajar@gmail.com>
2024-03-11 10:40:04 -0700

    JS0

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,12 +5,6 @@
 function add(a, b) {
   const num1 = parseInt(a);
   const num2 = parseInt(b);
-
-  if (isNaN(num1) || isNaN(num2)) {
-    console.log('NaN');
-    return;
-  }
-
   return num1 + num2;
 }
 
