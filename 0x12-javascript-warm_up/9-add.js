9296b3f8e538be20eff387585c373983c91516da
Soukaina111 <basmahajar@gmail.com>
2024-03-11 10:41:00 -0700

    JS0

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 // This function prints the addition of 2 integers
 
 function add(a, b) {
@@ -7,5 +6,4 @@ function add(a, b) {
   const num2 = parseInt(b);
   return num1 + num2;
 }
-
 console.log(add(process.argv[2], process.argv[3]));
