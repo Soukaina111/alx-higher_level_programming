675ebf652f3955fc2df8b22724aa991657a70f02
Soukaina111 <basmahajar@gmail.com>
2024-03-11 09:28:10 -0700

    JS0

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // This script prints what is requiered in the task
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
