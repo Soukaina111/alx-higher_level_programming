114a9d246dbc66ae97e182d86acbd7dbbdce85e5
Soukaina111 <basmahajar@gmail.com>
2024-03-11 10:38:48 -0700

    JS0

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// This function prints the addition of 2 integers
+
+function add(a, b) {
+  const num1 = parseInt(a);
+  const num2 = parseInt(b);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log('NaN');
+    return;
+  }
+
+  return num1 + num2;
+}
+
+console.log(add(process.argv[2], process.argv[3]));
