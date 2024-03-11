31c45eca05b00ffa6eec307037070e924685afe9
Soukaina111 <basmahajar@gmail.com>
2024-03-11 11:17:50 -0700

    JS0

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// This Script displays the standard inputin the givven format
+// This script displays the standard inputin the givven format
 
 if (process.argv.length > 3) {
   console.log(process.argv[2] + " is " + process.argv[3]);
