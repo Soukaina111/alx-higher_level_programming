077882618a5979016ddc68e3ad84fc1748c01a3d
Soukaina111 <basmahajar@gmail.com>
2024-03-11 11:19:16 -0700

    JS0

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,9 +2,9 @@
 // This script displays the standard inputin the givven format
 
 if (process.argv.length > 3) {
-  console.log(process.argv[2] + " is " + process.argv[3]);
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else if (process.argv.length === 3) {
-  console.log(process.argv[2] + " is undefined");
+  console.log(process.argv[2] + ' is undefined');
 } else {
-  console.log("undefined is undefined");
+  console.log('undefined is undefined');
 }
