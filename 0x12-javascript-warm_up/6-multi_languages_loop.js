8eca83386ca52767c8a784c2711d999d350ad2f4
Soukaina111 <basmahajar@gmail.com>
2024-03-11 10:10:07 -0700

    JS0

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// do the same as task 3 but with array and loop
+
+let myarr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < myarr.length; i++) {
+  console.log(myarr[i]);
+}
