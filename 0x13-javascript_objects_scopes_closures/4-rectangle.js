1d6dfb753d741ed06e26a36c7f803a61c6625e45
Soukaina111 <basmahajar@gmail.com>
2024-03-12 08:45:50 -0700

    REC

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,15 +2,13 @@
 // This script adds a rotate and double method to the previous code
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    } else {
-      this.height = h;
+
+ constructor (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
+      this.height = h;
     }
   }
-
   print() {
     for (let i = 0; i < this.height; i++) {
       let ro = "";
