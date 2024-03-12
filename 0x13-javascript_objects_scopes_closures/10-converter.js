21c8ceec6b3541688f700463a73f9598007ad0e3
Soukaina111 <basmahajar@gmail.com>
2024-03-12 08:55:51 -0700

    REC

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-//This script converts a number from base 10 to another base passed as argument
+// This script converts a number from base 10 to another base passed as argument
 
 exports.converter = function (base) {
   return function (number) {
