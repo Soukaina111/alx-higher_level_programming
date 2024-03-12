1f958a89fb0cd6aec32aaf1647969dfaad9ed555
Soukaina111 <basmahajar@gmail.com>
2024-03-12 08:50:26 -0700

    REC

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
       this.height = h;
     }
   }
- print() {
+ print(){
     for (let i = 0; i < this.height; i++) {
       let ro = "";
       for (let j = 0; j < this.width; j++) {
