4379cd25a94e2cb1ce5c9b324698e5c0b61a42fa
Soukaina111 <basmahajar@gmail.com>
2024-03-12 08:52:44 -0700

    REC

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// this script defines a subclass square of Rectnagle class
+// This script defines a subclass square of Rectnagle class
 
 const Rectangle = require('./4-rectangle.js');
-
 module.exports = class Square extends Rectangle {
   constructor(size) {
     super(size, size);
