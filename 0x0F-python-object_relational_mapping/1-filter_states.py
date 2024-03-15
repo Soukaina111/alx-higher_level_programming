59c38aa79fd945896aa2495fabbd6e6f479babc9
Soukaina111 <basmahajar@gmail.com>
2024-03-15 14:14:55 -0700

    ORM2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-""" 
+"""
 This script that lists all states from the database
 """
 
@@ -12,10 +11,17 @@ if __name__ == '__main__':
     user_name = argv[1]
     pass_word = argv[2]
     data_base = argv[3]
-
-    db = MySQLdb.connect(host = "localhost", port = 3306, user = user_name, passwd = pass_word, db = data_base)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user_name,
+        passwd=pass_word,
+        db=data_base,
+        charset="utf8"
+    )
     mapi = db.cursor()
-    mapi.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    mapi.execute("SELECT * FROM states WHERE name LIKE\
+            BINARY 'N%' ORDER BY id ASC")
     rows = mapi.fetchall()
     for data in rows:
         print(data)
