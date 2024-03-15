c4941856b9c23cb50d1dbc022659a9abe7fd53da
Soukaina111 <basmahajar@gmail.com>
2024-03-15 14:04:34 -0700

    ORM2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-""" 
-This script displays all states from the database
-"""
+""" This script displays all states from the database """
 import MySQLdb
 from sys import argv
 
+# To not be executed when imported we add this condition
 if __name__ == '__main__':
 
     # retrieve the arguments of CLI
-    user_name = argv[1]
-    pass_word = argv[2]
-    data_base = argv[3]
+	user_name = argv[1]
+    	pass_word = argv[2]
+    	data_base = argv[3]
+    	db = MySQLdb.connect(host="localhost", port=3306, user=user_name,
+                         passwd=pass_word, db=data_base)
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=user_name, passwd=pass_word, db=data_base,charset="utf8")
-    mapi = db.cursor()
+    
+        mapi = db.cursor()
+   	mapi.execute("SELECT * FROM states")
 
-    mapi.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = mapi.fetchall()
-    for data in rows:
-        print(data)
-    # Close connexion
-    mapi.close()
-    db.close()
+    	rows = mapi.fetchall()
+    	for data in rows:
+        	print(data)
+    	# Close connexion
+    	mapi.close()
+    	db.close()
