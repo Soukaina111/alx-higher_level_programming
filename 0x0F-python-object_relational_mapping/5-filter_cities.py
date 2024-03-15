7b2c57e0f5c874d5b626b89cdd22feb8671c91dd
Soukaina111 <basmahajar@gmail.com>
2024-03-15 15:14:48 -0700

    ORM2

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
+"""
+This script lists the cities of state passed as an argv
+"""
 import MySQLdb
 from sys import argv
 
-
 if __name__ == '__main__':
+    user_name = argv[1]
+    pass_word = argv[2]
+    data_base = argv[3]
+    state_n = argv[4]
 
-        user_name= argv[1]
-        pass_word = argv[2]
-	data_base= argv[3]
-	searched = argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, user=user_name,
+                         passwd=pass_word, db=data_base)
 
-	db= MySQLdb.connect(host="localhost", port = "3306", user=user_name, passwd= pass_word,db=data_base)
-
-	mapi = db.cursor();
-        mapi.execute("SELECT cities.id, cities.name FROM cities\
+    mapi = db.cursor()
+    mapi.execute("SELECT cities.id, cities.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s", (searched,))
+                WHERE states.name = %s", [state_n])
 
-        rows = mapi.fetchall()
-        listA = []
-        for i in rows:
-            listA.append(i[1])
-        print(", ".join(listA))
+    rows = mapi.fetchall()
+    ListA = []
+    for i in rows:
+        ListA.append(i[1])
+    print(", ".join(ListA))
 
-	mapi.close();
-	db.close();
+    mapi.close()
+    db.close()
