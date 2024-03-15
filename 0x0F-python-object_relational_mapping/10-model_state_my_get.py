543f74277dc8d699f2dd639c0b5f63b39981a416
Soukaina111 <basmahajar@gmail.com>
2024-03-15 14:41:29 -0700

    ORM2

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,13 +8,13 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == '__main__':
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
+    username = sys.argv[1]
+    password = sys.argv[2]
     db_name = sys.argv[3]
     searched = sys.argv[4]
 
-    engine = create_engine(f'mysql://{mysql_username}:{mysql_password}@localhost:3306/{db_name}')
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name))
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
