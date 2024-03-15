75d1dc4af719be4c367191539025f400f0a2c859
Soukaina111 <basmahajar@gmail.com>
2024-03-15 14:31:05 -0700

    ORM2

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-
-    engine = create_engine(f'mysql : //{username} : {password}@localhost : 3306/{db_name}')
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name))
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
