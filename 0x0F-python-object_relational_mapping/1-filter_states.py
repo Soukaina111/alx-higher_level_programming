d779f6f2926e3a4f22ec55c85a31370c2a669cc4
Soukaina111 <basmahajar@gmail.com>
2024-03-15 09:00:42 -0700

    ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-""" This script that lists all states from the database hbtn_0e_0_usa """
+""" 
+This script that lists all states from the database
+"""
 
 import MySQLdb
 from sys import argv
@@ -12,14 +14,10 @@ if __name__ == '__main__':
     data_base = argv[3]
 
     db = MySQLdb.connect(host = "localhost", port = 3306, user = user_name, passwd = pass_word, db = data_base)
-
     mapi = db.cursor()
-
     mapi.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = mapi.fetchall()
-
     for data in rows:
         print(data)
-
     mapi.close()
     db.close()
