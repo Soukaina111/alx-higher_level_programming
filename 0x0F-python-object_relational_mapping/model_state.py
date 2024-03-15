23bcb36d54996b6192908857145d75541b1750e6
Soukaina111 <basmahajar@gmail.com>
2024-03-15 08:29:01 -0700

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,6 @@ This script is  Using module SQLAlchemy
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
@@ -14,5 +13,3 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-    engine = create_engine('mysql://username:password@localhost:3306/hbtn_0e_4_usa')
-    Base.metadata.create_all(engine)
