636c127c5b4554ee8cd2a33bbd2ab8236fa7717a
Soukaina111 <basmahajar@gmail.com>
2024-03-15 08:27:22 -0700

    ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 	mapi = db.cursor();
         mapi.execute("SELECT cities.id, cities.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s", searched)
+                WHERE states.name = %s", (searched,))
 
         rows = mapi.fetchall()
         listA = []
