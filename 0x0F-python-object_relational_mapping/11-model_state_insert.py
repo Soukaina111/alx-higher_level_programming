874aff292da1bc42908bfdc3fe56e32344b13845
Soukaina111 <basmahajar@gmail.com>
2024-03-15 08:51:21 -0700

    ORM

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,18 +11,13 @@ from model_state import Base, State
 
 Base = declarative_base()
 
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
-
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
     engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{db_name}')
-    Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
