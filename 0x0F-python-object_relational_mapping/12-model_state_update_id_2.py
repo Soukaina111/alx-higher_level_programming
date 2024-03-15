526e580adc46acc30bd70ded15de289e5f7fb396
Soukaina111 <basmahajar@gmail.com>
2024-03-15 14:44:24 -0700

    ORM2

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script uses SQLAlchemy to change the name of a State object in the database
+This script uses SQLAlchemy to change the name
+of a State object in the database
 """
 
 import sys
@@ -12,17 +13,19 @@ from model_state import Base, State
 
 Base = declarative_base()
 
+
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-
-    engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{db_name}')
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name))
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
