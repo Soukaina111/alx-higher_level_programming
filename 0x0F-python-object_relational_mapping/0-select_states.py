7f240e2b08d92f5f740efb586e79d187c6e3af36
Soukaina111 <basmahajar@gmail.com>
2024-03-15 07:47:44 -0700

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,14 +7,19 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+
+    # retrieve the arguments of CLI
     user_name = argv[1]
     pass_word = argv[2]
     data_base = argv[3]
+    # Establish the connexion
     db = MySQLdb.connect(host="localhost", port=3306, user=user_name, passwd=pass_word, db=data_base)
     mapi = db.cursor()
+    # Execute the query
     mapi.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = mapi.fetchall()
     for data in rows:
         print(data)
+    # Close connexion
     mapi.close()
     db.close()
