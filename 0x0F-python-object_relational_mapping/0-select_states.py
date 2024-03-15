ed78858191b79d3c1efd9cf7174ffc51f704e5ae
Soukaina111 <basmahajar@gmail.com>
2024-03-15 14:11:56 -0700

    ORM2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 This script displays all states from the database
 """
 import MySQLdb
@@ -11,8 +11,14 @@ if __name__ == '__main__':
     user_name = argv[1]
     pass_word = argv[2]
     data_base = argv[3]
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=user_name, passwd=pass_word, db=data_base,charset="utf8")
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user_name,
+        passwd=pass_word,
+        db=data_base,
+        charset="utf8"
+    )
     mapi = db.cursor()
 
     mapi.execute("SELECT * FROM states ORDER BY states.id ASC")
@@ -22,4 +28,3 @@ if __name__ == '__main__':
     # Close connexion
     mapi.close()
     db.close()
-
