6b029a66c493598b1b32b16227f594fd2bac3363
Soukaina111 <basmahajar@gmail.com>
2024-03-15 14:26:32 -0700

    ORM2

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,17 +12,24 @@ import sys
 
 Base = declarative_base()
 
+
 class State(Base):
+
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == '__main__':
+
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{db_name}')
+    engine = create_engine(f'mysql : //{username} : \
+            {password}@localhost :\
+            3306/{db_name}')
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
@@ -30,6 +37,5 @@ if __name__ == '__main__':
 
     states = session.query(State).order_by(State.id.asc()).all()
     for state in states:
-         print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
-
