4ada0c80e11b3cb5408530574f401e4e1aaa56f1
Soukaina111 <basmahajar@gmail.com>
2024-03-15 07:43:16 -0700

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" This script displays all states from the database hbtn_0e_0_usa """
+""" 
+This script displays all states from the database hbtn_0e_0_usa 
+"""
 
 import MySQLdb
 from sys import argv
@@ -10,7 +12,7 @@ if __name__ == '__main__':
     data_base = argv[3]
     db = MySQLdb.connect(host="localhost", port=3306, user=user_name, passwd=pass_word, db=data_base)
     mapi = db.cursor()
-    mapi.execute("SELECT * FROM states ORDER BY id ASC")
+    mapi.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = mapi.fetchall()
     for data in rows:
         print(data)
