4dbbb41079bf76e3812c2da2041fcded009a8f9c
Soukaina111 <basmahajar@gmail.com>
2024-03-16 09:03:13 -0700

    ORM3

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 
-from sys import argv
+"""
+This script displays  all State objects and their
+corresponding City objects
+"""
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
-
-    engine = create_engine(f'mysql+mysqldb://{username}:\
-            {password}@localhost:3306/{database}')
-    Base.metadata.bind = engine
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-
-    states = session.query(State).order_by(State.id).all()
-
+    
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        db_username, db_password, db_name))
+    
+    Base.metadata.create_all(engine)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    states = session.query(State).all()
+    
     for st in states:
-        print(f'{st.id}: {st.name}')
+        print("{}: {}".format(st.id, st.name))
         for city in st.cities:
-            print(f'\t{city.id}: {city.name}')
-
+            print("\t{}: {}".format(city.id, city.name))
+    
     session.close()
