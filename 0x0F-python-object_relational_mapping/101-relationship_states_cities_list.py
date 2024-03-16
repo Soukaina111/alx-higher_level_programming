28646be66b6805ad3fb430951add06b58d0bf383
Soukaina111 <basmahajar@gmail.com>
2024-03-16 09:06:57 -0700

    ORM3

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,24 +12,17 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    
     db_username = sys.argv[1]
     db_password = sys.argv[2]
     db_name = sys.argv[3]
-    
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         db_username, db_password, db_name))
-    
     Base.metadata.create_all(engine)
-    
     Session = sessionmaker(bind=engine)
     session = Session()
-    
     states = session.query(State).all()
-    
     for st in states:
         print("{}: {}".format(st.id, st.name))
         for city in st.cities:
             print("\t{}: {}".format(city.id, city.name))
-    
     session.close()
