35f8984e7310aea5a7537782f43cc4446a3fc827
Soukaina111 <basmahajar@gmail.com>
2024-03-27 09:33:10 -0700

    0-body_size.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-vol=$(curl -sI $1 | awk '/Content-Length/ {print $2}')
-echo "Response body size: $size bytes"
+curl -sfL "$1" -X GET
