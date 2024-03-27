9494639b6d0ec8831cfe3377a8de2e1cc7c16106
Soukaina111 <basmahajar@gmail.com>
2024-03-27 09:32:09 -0700

    0-body_size.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-url="$1"
-outc=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-if [ "$outc" -eq 200 ]; then
-  curl -s "$url"
-else
-  echo "Error: Non-200 status code received. Status code: $outc"
-fi
-
+vol=$(curl -sI $1 | awk '/Content-Length/ {print $2}')
+echo "Response body size: $size bytes"
