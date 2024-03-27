df64628dec7135159ca146ad1f7842b3607d1805
Soukaina111 <basmahajar@gmail.com>
2024-03-27 09:45:07 -0700

    0-body_size.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script  takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-header="X-School-User-Id: 98"
-curl -sH "$header" $1
+# This script takes in a URL as an argument, sends a GET request to that URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
