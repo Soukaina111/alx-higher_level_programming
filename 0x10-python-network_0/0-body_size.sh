3f5981f8195903450e502d43ca6533a82630f1e2
Soukaina111 <basmahajar@gmail.com>
2024-03-27 09:10:52 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,6 +2,5 @@
 # This script resent the  request to the same URlthat it come from
 # While displaying the size of the response's body
  
-url1= $1
 
-curl -sI url1 | grep "Content-Length" | cut -d " " -f2
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
