e920adf5bd471e35afb54644f69ec76eb60f5b47
Soukaina111 <basmahajar@gmail.com>
2024-03-27 09:57:23 -0700

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,17 +2,32 @@
 """
 Script/Function for task 6 about Peak
 """
-
 def find_peak(list_of_integers):
-    left = 0
-    right = len(list_of_integers) - 1
+    def case_0():
+        return None
 
-    while left < right:
-        mid = (left + right) // 2
+    def case_1():
+        return list_of_integers[0]
 
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
+    def case_2():
+        return max(list_of_integers)
+
+    def case_default():
+        mid = len(list_of_integers) // 2
+        peak = list_of_integers[mid]
+
+        if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+            return peak
+        elif peak < list_of_integers[mid - 1]:
+            return find_peak(list_of_integers[:mid])
         else:
-            right = mid
+            return find_peak(list_of_integers[mid + 1:])
 
-    return list_of_integers[left]
+    switch = {
+        0: case_0,
+        1: case_1,
+        2: case_2,
+    }
+
+    switch_case = switch.get(len(list_of_integers), case_default)
+    return switch_case()
