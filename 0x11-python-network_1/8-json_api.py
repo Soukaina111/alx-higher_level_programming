0672f9b58ed522958766cf04866c1682d0929399
Soukaina111 <basmahajar@gmail.com>
2024-03-28 10:05:26 -0700

    To 7

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+     This script takes in a letter and sends a POST request to
+     http://0.0.0.0:5000/search_user
+     with the letter as a parameter
+"""
+from sys import argv
+import requests
+
+
+def send_post_request(Link, charge):
+    resp = requests.post(Link, data=charge)
+    return resp
+
+
+def process_response(response):
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            if data:
+                print("[{}] {}".format(data.get("id"), data.get("name")))
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+    else:
+        print("Error code:", response.status_code)
+
+
+if __name__ == "__main__":
+    Lin = "http://0.0.0.0:5000/search_user"
+    if len(argv) < 2:
+        word = ""
+    else:
+        word = argv[1]
+    char = {"q": letter}
+    respo = send_post_request(Lin, char)
+    process_response(respo)
