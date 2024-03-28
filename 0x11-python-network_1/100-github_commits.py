c8b9c2e4b517261e7794b3cc6155e1f01916d875
Soukaina111 <basmahajar@gmail.com>
2024-03-28 10:30:47 -0700

    To 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+ This script answers the "TIME FOR AN INTERVIEW "
+ task
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    repo_name = argv[1]
+    repo_owner = argv[2]
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
+
+    response = requests.get(url)
+    commits = response.json()
+    for co in commits[:10]:
+        sha = co["sha"]
+        author_name = com["commit"]["author"]["name"]
+        print(f"{sha}: {author_name}")
