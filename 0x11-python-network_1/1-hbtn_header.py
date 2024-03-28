68f8844a1ae35b542c87a51d6838b0eaa784839e
Soukaina111 <basmahajar@gmail.com>
2024-03-28 08:52:36 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,9 @@
-from urllib.request import Request, urlopen
-from sys import argv
+#!/usr/bin/python3
 """ This script  sends a request to the URL and displays the value
 of the X-Request-Id variable found in the header of the response."""
+
+from urllib.request import Request, urlopen
+from sys import argv
 
 if __name__ == "__main__":
     Link = argv[1]
