2291ecb0f90e0293b90a571c1a0634d763ccf8c3
Soukaina111 <basmahajar@gmail.com>
2024-03-28 10:20:43 -0700

    To 10

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,9 +10,9 @@ from sys import argv
 if __name__ == "__main__":
     Link = argv[1]
     req = requests.get(Link)
-    req_status = req.status_code
+    reqe_status = req.status_code
 
-    if req_status < 400:
+    if reqe_status < 400:
         print(req.text)
     else:
-        print("Error code:", req.status)
+        print("Error code:", reqe.status)
