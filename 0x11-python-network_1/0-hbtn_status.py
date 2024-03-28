4f77ff310470284b21e122fb9d7eb3c21bd39d96
Soukaina111 <basmahajar@gmail.com>
2024-03-28 08:42:29 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,14 @@
 from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-
-	req = Request('https://alx-intranet.hbtn.io/status')
-	with urlopen(req) as res:
-		 html = res.read()
-		 print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-                         .format(type(html), html, html.decode("utf-8")))
+    req = Request('https://alx-intranet.hbtn.io/status')
+    with urlopen(req) as res:
+        html = res.read()
+        print(
+            "Body response:\n"
+            "\t- type: {}\n"
+            "\t- content: {}\n"
+            "\t- utf8 content: {}".format(
+                type(html), html, html.decode("utf-8")
+            )
+        )
