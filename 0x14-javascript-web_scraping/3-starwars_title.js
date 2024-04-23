27722698159ae0421477dbdc17e10e17c1addcfa
Soukaina111 <basmahajar@gmail.com>
2024-04-23 10:02:58 -0700

    semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,16 +2,9 @@
 
 const request = require('request');
 
-if (process.argv.length < 3) {
-    console.error('Please provide a movie ID as an argument.');
-    process.exit(1);
-}
+const starWarsUri = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request.get(url, (error, response, body) => {
+request(starWarsUri, (error, response, body) => {
     if (error) {
         console.error('An error occurred:', error);
         process.exit(1);
