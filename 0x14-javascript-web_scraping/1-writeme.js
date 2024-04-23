38ed97e2396b07c3726551073e22ee68f2f4cce6
Soukaina111 <basmahajar@gmail.com>
2024-04-23 09:38:44 -0700

    semistandard

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// this one writes the second arg
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  console.error('Please provide a file path and a string to write as arguments.');
+  process.exit(1);
+}
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filePath, data, 'utf-8', (err) => {
+  if (err) {
+    console.error('An error occurred:', err);
+  } else {
+    console.log('File written successfully.');
+  }
+});
