1605d19f5ea46f84d107ebb925a951c4cd5a67a5
Soukaina111 <basmahajar@gmail.com>
2024-04-23 08:57:18 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// this js script to read content of a file 
+
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+    console.error('Please provide a file path as an argument.');
+    process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error('An error occurred:', err);
+    } else {
+        console.log(data);
+    }
+});
+
