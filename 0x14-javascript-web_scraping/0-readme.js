09258caed7c18e5d7b898e7e2cdd8c27b45cbd68
Soukaina111 <basmahajar@gmail.com>
2024-04-23 09:00:58 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -13,9 +13,9 @@ const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-        console.error('An error occurred:', err);
+	    console.error('An error occurred:', err);
     } else {
-        console.log(data);
+	    console.log(data);
     }
 });
 
