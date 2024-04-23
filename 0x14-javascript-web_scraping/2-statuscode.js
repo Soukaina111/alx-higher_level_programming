f538cd4eb37dba53922225104f7270e8d652d679
Soukaina111 <basmahajar@gmail.com>
2024-04-23 09:56:07 -0700

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,17 +3,16 @@
 const request = require('request');
 
 if (process.argv.length < 3) {
-    console.error('Please provide a URL as an argument.');
-    process.exit(1);
+  console.error('Please provide a URL as an argument.');
+  process.exit(1);
 }
 
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error('An error occurred:', error);
-        process.exit(1);
-    }
-   console.log(`code: ${response.statusCode}`);
+  if (error) {
+    console.error('An error occurred:', error);
+    process.exit(1);
+  }
+  console.log(`code: ${response.statusCode}`);
 });
-
